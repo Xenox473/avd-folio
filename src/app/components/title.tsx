ac7223702eb962b258893451701b03d2ee25c4fd
Abhishek Dhar <abhishek473@gmail.com>
2024-10-03 14:39:31 -0400

    Updated landing page styles

--- src/app/components/title.tsx
+++ src/app/components/title.tsx
@@ -1,20 +1,29 @@
 import { Stack, Typography } from "@mui/material"
 
+const spanStyles = {
+  margin: '0.5rem',
+}
+
 export const Title = () => {
 
   return (
-    <Stack spacing={2} sx={{ flex: 1, display: 'flex', alignContent: 'center', justifyContent: 'center' }}>
-      <Typography variant="h1" fontSize={64} fontFamily={'monospace'}>
-        Abhi V Dhar 
-        <Typography 
-          variant="caption"
-          fontSize={16}
-          fontFamily={'monospace'}> 
-          (uh-bee)
-        </Typography>
+    <Stack spacing={2} sx={{ alignItems: 'center' }}>
+      <Typography variant="h1" fontSize={'4rem'} fontFamily={'monospace'}>
+        <div style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        }}>
+          <span style={spanStyles}>Abhi</span>
+          <span style={spanStyles}>V</span>
+          <span style={spanStyles}>Dhar</span>
+        </div>
       </Typography>
-      
-      <Typography variant="h4" fontSize={32}>Sr. Software Engineer</Typography>
+      <Typography variant="h4" fontSize={'2rem'}>
+        Sr. Software Engineer
+      </Typography>
     </Stack>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,47 @@
 import { Title } from "./components/title";
-import { Timeline } from './components/timeline';
-import { Blurb } from './components/blurb';
-import LinkDisplay from './components/LinkDisplay';
+// import { Timeline } from './components/timeline';
+// import { Blurb } from './components/blurb';
+// import LinkDisplay from './components/LinkDisplay';
+import ResponsiveAppBar from "./components/ResponsiveAppBar";
 
 export default function Home() {
   return (
     <>
-      <div style={{ display: 'flex', flexDirection: 'column', height: '100%', justifyContent: 'center' }}>
-        <div style={{ display: 'flex', justifyContent: 'space-around', flex: 1, margin: "1rem" }}>
-          <Title />
-          <LinkDisplay />
-        </div>
-        <div style={{display: 'flex', flexWrap: 'wrap', flex: 9}}>
-          <div style={{ 
-            flex: 1, 
-            display: 'flex', 
-            flexDirection: 'column', 
-            justifyContent: 'space-around',
-            alignContent: 'center',
-            flexWrap: 'wrap'
-          }}>
-            <div style={{ display: 'flex', maxWidth: '75%' }}>
-              <Blurb />
-            </div>
-            <div>spotify stuff here!!!</div>
-          </div>
-          <div style={{
-            flex: 1,
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignContent: 'center',
-            flexWrap: 'wrap'
-          }}>
-            <Timeline />
-          </div>
-        </div>
+      <ResponsiveAppBar />
+      <div style={{ 
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        height: '100vh',
+      }}>
+        <Title />
       </div>
+      {/* <div style={{display: 'flex', flexWrap: 'wrap', flex: 9}}>
+        <div style={{ 
+          flex: 1, 
+          display: 'flex', 
+          flexDirection: 'column', 
+          justifyContent: 'space-around',
+          alignContent: 'center',
+          flexWrap: 'wrap'
+        }}>
+          <div style={{ display: 'flex', maxWidth: '75%' }}>
+            <Blurb />
+          </div>
+          <div>spotify stuff here!!!</div>
+        </div>
+        <div style={{
+          flex: 1,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignContent: 'center',
+          flexWrap: 'wrap'
+        }}>
+          <Timeline />
+        </div>
+      </div> */}
     </>
   );
 }
