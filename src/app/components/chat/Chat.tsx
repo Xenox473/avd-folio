fcc37b69ad4a9b94b0a5874fd5974209df5c3b94
Abhishek Dhar <abhishek473@gmail.com>
2024-11-16 13:48:25 -0500

    Added loading state

--- src/app/components/chat/Chat.tsx
+++ src/app/components/chat/Chat.tsx
@@ -1,4 +1,4 @@
-import { FormControl, IconButton, Input, InputLabel } from "@mui/material";
+import { FormControl, IconButton, Input, InputLabel, Skeleton } from "@mui/material";
 import SendIcon from '@mui/icons-material/Send';
 import { colors } from "@/app/colors";
 import { useState } from "react";
@@ -12,12 +12,14 @@ type MessageProps = {
 const Chat = () => {
   const [ question, setQuestion ] = useState('');
   const [ messageHistory, setMessageHistory ] = useState<MessageProps[]>([]);
+  const [ isLoading, setIsLoading ] = useState<boolean>(false);
 
   async function handleSubmit() {
     const newMessageHistory = [...messageHistory, { sender: "user", message: question }]
     
     setMessageHistory(newMessageHistory)
     setQuestion('');
+    setIsLoading(true);
 
     fetch('/api/chat', {
       method: 'POST',
@@ -30,7 +32,7 @@ const Chat = () => {
       response.json().then((answer) => {
         setMessageHistory([...newMessageHistory, { sender: "bot", message: answer }])
       })
-    })
+    }).then(() => { setIsLoading(false) })
   };
 
   const handleKeyDown = (event) => {
@@ -58,23 +60,26 @@ const Chat = () => {
         flex: 12,
         gap: 5,
         height: "100%",
-        overflowY: "scroll"
+        overflowY: "scroll",
+        padding: "8px"
       }}>
         { messageHistory.map((message) => (
           <Message key={message.message} message={message.message} sender={message.sender} />
         ))}
+        { isLoading && <Skeleton variant="rounded" height={64} width="50%"/>}
       </div>
       <div style={{ width: '100%', padding: '8px', flex: 1 }}>
         <FormControl 
           fullWidth
           sx={{ display: 'flex', flexDirection: 'row' }}
         >
-          <InputLabel htmlFor="my-input">Ask a question</InputLabel>
+          <InputLabel>Ask a question</InputLabel>
           <Input
             sx={{ flex: 2 }}
             value={question}
             onChange={(event) => setQuestion(event.target.value)}
             onKeyDown={(event) => handleKeyDown(event)}
+            disabled={isLoading}
           />
           <IconButton type="submit" size="large" onClick={() => handleSubmit()}>
             <SendIcon />
